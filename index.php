6e327d25640f9e6697bfe36040ba303d56508df1
Krystian Czajkowski <czajkowski.krystian@gmail.com>
2019-12-29 06:47:32 +0100

    Change way of showing info about cookies

--- index.php
+++ index.php
@@ -69,6 +69,34 @@
     <!-- Including footer -->
     <?php include 'includes/footer.php'; ?>
    
+
+    <?php 
+        //Setting cookie to not show cookie popup and we are refreshing page
+        if(isset($_GET['accept-cookies'])){
+            setcookie('accept-cookies', 'true', time() + 31556925);
+            echo("<script>location.href = '".BASE_URL.$profileData->screenName."'</script>");
+        }
+    ?>
+    
+    <!-- This website is using cookies information here -->
+    <?php if(!$functions->loggedIn() && !isset($_COOKIE['accept-cookies'])) {  ?>
+    
+    <div style='margin-top: 150px;'></div>
+    <div class='alert alert-dark bg-light text-black alert-dismissable fixed-bottom m-0'>
+        <div class="container">
+                  
+            <div class="d-flex">
+                <span style='font-size: 1.1rem;' class='pt-1'>SocialsHub.net uses cookies to give you the best possible experience.<a href="privacy-policy.php"> Read More</a></span>
+                
+                <!-- Accept cookie button -->
+                <a href="?accept-cookies" class='btn btn-success font-weight-bold ml-auto' style='font-size: 1.1rem;'>OK</a>
+            </div>
+            
+        </div>
+    </div>
+    <?php } ?>
+
+
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>

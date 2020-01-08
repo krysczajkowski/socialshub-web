a5f4d109e7e8a22504d7f3f24c3501f8e818033e
Krystian Czajkowski <czajkowski.krystian@gmail.com>
2020-01-08 16:24:33 +0100

    Change look of info about cookies

--- includes/cookie-info.php
+++ includes/cookie-info.php
@@ -8,7 +8,7 @@
             <span style='font-size: 1.1rem;' class='pt-1'>SocialsHub.net uses cookies to give you the best possible experience.<a href="privacy-policy.php"> Read More</a></span>
             
             <!-- Accept cookie button -->
-            <a href="?accept-cookies" class='btn btn-success font-weight-bold ml-auto' style='font-size: 1.1rem;'>OK</a>
+            <a href="?accept-cookies" class='btn btn-outline-primary font-weight-bold ml-auto my-auto' style='font-size: 1rem;'>Accept</a>
         </div>
         
     </div>
--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
         <div class="col-3 col-lg-1 my-2"><a href='terms.php' class='font-weight-bold text-muted link'>Terms</a></div>
         <div class="col-lg-3 my-2 offset-6">
             <div class="d-flex">
-                <span class="ml-auto font-weight-bold text-muted">&copy; 2019 SocialsHub</span>
+                <span class="ml-auto font-weight-bold text-muted">&copy; 2020 SocialsHub</span>
             </div>
         </div>
     </div>

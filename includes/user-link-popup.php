5bbec8c3dfb487c0d6c0a5a633a2284183f283ec
Krystian Czajkowski <czajkowski.krystian@gmail.com>
2020-02-17 06:29:59 +0100

    Added if you click your link in user-link-popup.php it copy to clipboard

--- includes/user-link-popup.php
+++ includes/user-link-popup.php
@@ -1,5 +1,16 @@
 <div class="alert alert-light text-center border-bottom">
-    <span class='font-weight-bold super-small-font text-muted'>Share Your Link</span>
+    <span class='font-weight-bold super-small-font text-muted'>Share Your Link (click it to copy)</span>
     <br>
-    <span class='link-color small-font'>https://socialshub.net/<?php echo $user->screenName; ?></span>
+    <span class='link-color small-font py-3' id="clickToCopyDiv" onclick="copyToClipboard()">https://socialshub.net/<?php echo $user->screenName; ?></span>
 </div>
+
+<script>
+    function copyToClipboard() {
+        var range = document.createRange();
+        range.selectNode(document.getElementById("clickToCopyDiv"));
+        window.getSelection().removeAllRanges(); // clear current selection
+        window.getSelection().addRange(range); // to select text
+        document.execCommand("copy");
+        window.getSelection().removeAllRanges();// to deselect
+    }
+</script>
